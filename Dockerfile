4588550d5f69530aeeae9344d44134d6b1868de1
Lassi Paavolainen <lassi.paavolainen@gmail.com>
2020-12-01 00:44:51 +0200

    Add workflow files

--- Dockerfile
+++ Dockerfile
@@ -7,7 +7,7 @@ RUN git clone https://github.com/cytomine-uliege/Cytomine-python-client.git && \
     rm -r /Cytomine-python-client
 
 # ------------------------------------------------------------------------------
-# Install Biaflows-Utilities (annotation exporter, compute metrics, helpers,...)
+# Install Neubias-W5-Utilities (annotation exporter, compute metrics, helpers,...)
 RUN apt-get update && apt-get install libgeos-dev -y && apt-get clean
 RUN git clone https://github.com/Neubias-WG5/biaflows-utilities.git && \
     cd /biaflows-utilities/ && git checkout tags/v0.9.1 && pip install .
@@ -19,6 +19,27 @@ RUN cp /biaflows-utilities/bin/* /usr/bin/ && \
 
 # ------------------------------------------------------------------------------
 
+RUN pip install scikit-learn joblib
+RUN pip install h5py==2.9.0
+RUN pip install keras==2.3.1
+RUN pip install tensorflow==1.13.1
+
+RUN git clone https://github.com/matterport/Mask_RCNN.git
+
+RUN cd Mask_RCNN && \
+    pip install -r requirements.txt && \
+    python setup.py install
+
+RUN pip install gdown
+
+RUN mkdir /app && \
+    cd /app && \
+    gdown https://drive.google.com/uc?id=19EmZ57LXSArG-Z1HC8NOtrQUxGNzW3vv
+
+RUN chmod 444 /app/weights.h5
+
 ADD wrapper.py /app/wrapper.py
+
+ADD maskrcnn_utils.py /app/maskrcnn_utils.py
 
 ENTRYPOINT ["python3.6","/app/wrapper.py"]
--- wrapper.py
+++ wrapper.py
@@ -1,26 +1,47 @@
 import sys
 import os
+import skimage
+import numpy as np
+from maskrcnn_utils import InferenceConfig
+from maskrcnn_utils import Dataset
+from mrcnn import model as modellib
 from cytomine.models import Job
-from biaflows import CLASS_OBJSEG, CLASS_SPTCNT, CLASS_PIXCLA, CLASS_TRETRC, CLASS_LOOTRC, CLASS_OBJDET, CLASS_PRTTRK, CLASS_OBJTRK
-from biaflows.helpers import BiaflowsJob, prepare_data, upload_data, upload_metrics, get_discipline
+from biaflows import CLASS_OBJSEG
+from biaflows.helpers import BiaflowsJob, prepare_data, upload_data, upload_metrics
 
 
 def main(argv):
     base_path = "{}".format(os.getenv("HOME")) # Mandatory for Singularity
-    
+    problem_cls = CLASS_OBJSEG
+
     with BiaflowsJob.from_cli(argv) as bj:
-        # Change following to the actual problem class of the workflow
-        problem_cls = get_discipline(bj, default=CLASS_OBJSEG)
-        
         bj.job.update(status=Job.RUNNING, progress=0, statusComment="Initialisation...")
-        
         # 1. Prepare data for workflow
         in_imgs, gt_imgs, in_path, gt_path, out_path, tmp_path = prepare_data(problem_cls, bj, is_2d=True, **bj.flags)
+        files = [image.filepath for image in in_imgs]
 
-        # 2. Run image analysis workflow
+        # 2. Run Mask R-CNN prediction
         bj.job.update(progress=25, statusComment="Launching workflow...")
 
-        # Add here the code for running the analysis script
+        model_dir = "/app"
+        dataset = Dataset()
+        dataset.load_files(files)
+        dataset.prepare()
+        inference_config = InferenceConfig()
+        model = modellib.MaskRCNN(mode = "inference",
+                                  config = inference_config,
+                                  model_dir = model_dir)
+        model.load_weights(os.path.join(model_dir,'weights.h5'), by_name=True)
+
+        for i,image_id in enumerate(dataset.image_ids):
+            tiles = dataset.load_image(image_id, bj.parameters.nuclei_major_axis)
+            tile_masks = []
+            for image in tiles:
+                mask = model.detect([image], verbose=0)[0]
+                tile_masks.append(mask)
+
+            mask_img = dataset.merge_tiles(image_id, tile_masks)
+            skimage.io.imsave(os.path.join(out_path,os.path.basename(files[i])), mask_img)
 
         # 3. Upload data to BIAFLOWS
         upload_data(problem_cls, bj, in_imgs, out_path, **bj.flags, monitor_params={
